16a2917b06b646ad6cdd0972e72e5c65872cf051
ahmedswidan1 <ahmedswidan221@yahoo.com>
2023-03-21 16:54:05 -0700

    hello

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,7 +7,7 @@
  * @n: the input number as an integer.
  *
  * Return: 1 is greater than zero. 0 is zero.
- * -1 is less than zero. 
+ * -1 is less than zero.
  */
 int print_sign(int n)
 {
@@ -17,7 +17,7 @@ int print_sign(int n)
 		return (1);
 	}
 	else if (n < 0)
-	{	
+	{
 		_putchar(45);
 		return (-1);
 	}
